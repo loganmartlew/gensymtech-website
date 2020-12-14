6e17f60b944f92ed340786186462bba1d1e22e41
Logan Martlew <logan.martlew@gmail.com>
2020-12-14 15:27:19 +1300

    card dom and styles

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+import 'simplebar';
+import 'simplebar/dist/simplebar.css';
+import 'material-design-icons/iconfont/material-icons.css';
 import './styles/style.scss';
+
+// import ResizeObserver from 'resize-observer-polyfill';
+// window.ResizeObserver = ResizeObserver;
 
 console.log('Hello world');
